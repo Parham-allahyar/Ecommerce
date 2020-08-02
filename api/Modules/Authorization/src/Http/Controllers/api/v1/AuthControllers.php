bf8e7808b17ca30dbc7d2de46f6a4df5af6d2dd7
Parham <parhamallahyar70@gmail.com>
2020-08-02 13:18:18 +0430

    feat(Modules/Authorization/src/Http/Controllers/api/v1/AuthControllers.php) GET User Number

--- api/Modules/Authorization/src/Http/Controllers/api/v1/AuthControllers.php
+++ api/Modules/Authorization/src/Http/Controllers/api/v1/AuthControllers.php
@@ -2,14 +2,25 @@
 
 namespace Authorization\Http\Controllers\api\v1;
 
-use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\Auth;
-
-
+use Illuminate\Http\Request;
 class AuthController extends Controller
 {
 
-    
+    public function login(Request $request)
+    {
+      //GET User Phone Number
+      $phoneNumber = $request->input('phone');
+
+      //Validation Phone Number
+
+      
+
+    }
+    public function auth()
+    {
+
+    }
 
 }
