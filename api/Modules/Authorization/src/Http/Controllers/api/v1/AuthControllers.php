ef7ee2919f5dafe82bd2400f2af37012d8e2330e
Parham <parhamallahyar70@gmail.com>
2020-08-02 12:17:30 +0430

    feat(Modules/Authorization/src/Http/Controllers/api/v1/AuthController) Create AuthController

---
+++ api/Modules/Authorization/src/Http/Controllers/api/v1/AuthControllers.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Authorization\Http\Controllers\api\v1;
+
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\Auth;
+
+
+class AuthController extends Controller
+{
+
+    
+
+}
---
+++ api/Modules/Authorization/src/Providers/AuthorizationServiceProvider.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Authorization\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class AuthorizationServiceProvider extends ServiceProvider
+{
+
+    public function register()
+    {
+        //
+    }
+
+
+    public function boot()
+    {
+        //
+    }
+}
