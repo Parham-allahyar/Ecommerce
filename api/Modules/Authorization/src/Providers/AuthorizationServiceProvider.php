3967acf97b436cee519ec6ed0a76548873f99e86
Parham <parhamallahyar70@gmail.com>
2020-08-02 14:35:15 +0430

    feat(Modules/Authorization/src/Providers/AuthorizationServiceProvider.php) Set implementation code for StoreCodeFacade

--- api/Modules/Authorization/src/Providers/AuthorizationServiceProvider.php
+++ api/Modules/Authorization/src/Providers/AuthorizationServiceProvider.php
@@ -5,13 +5,16 @@ namespace Authorization\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Route;
 use Authorization\Facades\userProviderFacade;
+use Authorization\Facades\storeCodeFacade;
 use Authorization\Repositories\UserRepository;
+use Authorization\PhpClass\storeCode;
 class AuthorizationServiceProvider extends ServiceProvider
 {
 
     public function register()
     {
         userProviderFacade::ShouldProxyTo(UserRepository::class);
+        storeCodeFacade::ShouldProxyTo(storeCode::class);
     }
 
 
